f714c2d372f2ed4f0782da773e2670841739f890
Angarius <angelo.prebianca@gmail.com>
2025-08-21 19:12:41 -0300

    feat: :sparkles: Adicionado esqueleto pagina 404 e alguns fixes para o Firefox

--- web/src/app/pages/errors/not-found/not-found.component.ts
+++ web/src/app/pages/errors/not-found/not-found.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './not-found.component.html',
   styles: ``
 })
