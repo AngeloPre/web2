1c16d5d9dd5ea0dd37e6334b8f3b9216688ecd3e
Angarius <angelo.prebianca@gmail.com>
2025-08-23 17:46:51 -0300

    feat: :sparkles: Finalizada pagina 404 com redirect
    Finalizada pagina 404 com redirect, adicionado botão com outline, atualizado pasta dos guards, instalado ngrx, criado rotas por usuario

--- web/src/app/pages/errors/not-found/not-found.component.ts
+++ web/src/app/pages/errors/not-found/not-found.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { UserRole } from '@core/store/user-role/user-role.store';
 
 @Component({
   selector: 'app-not-found',
-  imports: [MatButtonModule],
+  standalone: true,
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './not-found.component.html',
   styles: ``
 })
 export class NotFoundComponent {
-
+  readonly userRole = inject(UserRole);
 }
