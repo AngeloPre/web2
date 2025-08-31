a04da2a836970fe281f05f2f958384c0ac90d332
Mr-RenatoBlue <renatomedeirosrocha@gmail.com>
2025-08-31 19:23:20 -0300

    feat :lipstick:
    Adicionado service pra pegar endereço no viacep
    Herança simples para usuario/cliente/funcionario

---
+++ web/src/app/model/roles.ts
@@ -0,0 +1,4 @@
+import type { Role } from '../core/store/user-role/user-role.store';
+
+export const EMPLOYEE_ROLE: Role = { id: 1, name: 'employee' };
+export const CLIENT_ROLE: Role = { id: 2, name: 'client' };
