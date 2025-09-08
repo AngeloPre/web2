58043b3091a14f19a9c8809e3df0f34fb03fcd38
Angarius <angelo.prebianca@gmail.com>
2025-09-08 01:47:41 -0300

    feat: :sparkles: Adicionado a lista de equipamentos a condicional @empty, e os botões de mudar o status

--- web/src/app/shared/components/categoria-equipamento-table/categoria-equipamento-table.component.ts
+++ web/src/app/shared/components/categoria-equipamento-table/categoria-equipamento-table.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { StatusAtivoInativoComponent } from '../status-ativo-inativo/status-ativo-inativo.component';
 import { CategoriaEquipamento } from '@/app/model/categoria-equipamento.type';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-categoria-equipamento-table',
-  imports: [StatusAtivoInativoComponent],
+  imports: [StatusAtivoInativoComponent, MatButtonModule],
   templateUrl: './categoria-equipamento-table.component.html',
   styles: ``
 })
