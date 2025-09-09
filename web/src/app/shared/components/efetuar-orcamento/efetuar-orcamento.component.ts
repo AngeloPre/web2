592b0d1850ba6dfe45ba6a7a7b00c4bebf79acaf
João Mota <jvmotadev67@gmail.com>
2025-09-09 16:42:02 -0300

    feat: atualizando pag efetuar orcamento
    inserido o campo de serviços adicionais

--- web/src/app/shared/components/efetuar-orcamento/efetuar-orcamento.component.ts
+++ web/src/app/shared/components/efetuar-orcamento/efetuar-orcamento.component.ts
@@ -1,8 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
 import { FormsModule } from "@angular/forms";
 import { MatButton } from "@angular/material/button";
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ServicoAdicionalDialogComponent } from '../dialogs/servico-adicional-dialog/servico-adicional-dialog.component';
+import { ConfirmarOrcamentoDialogComponent } from '../dialogs/confirmar-orcamento-dialog/confirmar-orcamento-dialog.component';
 
 
 @Component({
@@ -12,5 +16,27 @@ import { MatButton } from "@angular/material/button";
   styles: ``
 })
 export class EfetuarOrcamentoComponent {
+  private dialog = inject(MatDialog);
+
+  adicionarServico() {
+    const ref = this.dialog.open(ServicoAdicionalDialogComponent, {
+          width: '440px',
+          maxWidth: 'none',
+          panelClass: 'dialog-xxl'
+        });
+  }
+
+  excluirServico() {
+    //implementar on delete do servico
+  }
+
+  efetuarOrcamento() {
+    const ref = this.dialog.open(ConfirmarOrcamentoDialogComponent, {
+          width: '440px',
+          maxWidth: 'none',
+          panelClass: 'dialog-xxl'
+        });
+  }
+
 
 }
