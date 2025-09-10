7b0bb372986b6c156fdeb37977f1f0c786c3d851
Henrique <henriquekkrrr@gmail.com>
2025-09-09 21:58:39 -0300

    bugfix : categoria equipamento listarPorStatus() agora funciona

--- web/src/app/services/categoria-equipamento.service.ts
+++ web/src/app/services/categoria-equipamento.service.ts
@@ -6,18 +6,20 @@ import { StatusAtivoInativo } from '../model/enums/status-ativo-inativo.enum';
 export const LS_CategoriaEquipamento = 'CategoriaEquipamento';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CategoriaEquipamentoService implements MockServices<CategoriaEquipamento> {
+export class CategoriaEquipamentoService
+  implements MockServices<CategoriaEquipamento>
+{
   private categoriaID = 1;
   listarTodos(): CategoriaEquipamento[] {
     const categorias = localStorage[LS_CategoriaEquipamento];
-    return categorias ? (JSON.parse(categorias)) : [];
+    return categorias ? JSON.parse(categorias) : [];
   }
   listarPorStatus(status: StatusAtivoInativo): CategoriaEquipamento[] {
     const categorias = localStorage[LS_CategoriaEquipamento];
     const lista: CategoriaEquipamento[] = JSON.parse(categorias);
-    return lista.filter((categoria) => categorias.status === status);
+    return lista.filter((categoria) => categoria.isActive === status);
   }
   inserir(elemento: CategoriaEquipamento): void {
     const categorias = this.listarTodos();
@@ -43,7 +45,7 @@ export class CategoriaEquipamentoService implements MockServices<CategoriaEquipa
     localStorage[LS_CategoriaEquipamento] = JSON.stringify(categorias);
   }
 
-  constructor() { 
+  constructor() {
     if (this.listarTodos().length === 0) {
       this.inserir({
         id: 1,
