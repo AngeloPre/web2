710e7be847f4bd101b28c649209dddf8d5779fb6
Henrique <henriquekkrrr@gmail.com>
2025-09-14 17:50:35 -0300

    pag de solicitações funcionário com filtro de data

--- web/src/app/pages/pag-solicitacoes/pag-solicitacoes.component.ts
+++ web/src/app/pages/pag-solicitacoes/pag-solicitacoes.component.ts
@@ -1,26 +1,88 @@
 import { ChamadoCardComponent } from '@/app/shared/components/chamado-card/chamado-card.component';
-import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  Signal,
+  signal,
+} from '@angular/core';
 import { ChamadoItem } from '@/app/model/chamado-list.type';
-//Temporário
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatButton } from '@angular/material/button';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { ChamadoService } from '@/app/services/chamado.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-pag-solicitacoes',
-  imports: [ChamadoCardComponent],
+  providers: [
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+  ],
+  imports: [
+    ChamadoCardComponent,
+    MatButton,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './pag-solicitacoes.component.html',
-  styles: ``
+  styles: ``,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class PagSolicitacoesComponent implements OnInit {
+export class PagSolicitacoesComponent {
   private chamadoService = inject(ChamadoService);
+  opcaoData = signal<'HOJE' | 'DATA' | 'TODOS'>('TODOS');
+  data_inicial = signal<string>('');
+  data_fim = signal<string>('');
 
-  chamadosMock = signal<ChamadoItem[]>([]);
+  range = new FormGroup({
+    start: new FormControl<Date | null>(null),
+    end: new FormControl<Date | null>(null),
+  });
 
-  ngOnInit(): void {
-    this.atualizarTela();
-    console.log(this.chamadoService.listarTodos());
+  chamdados: Signal<ChamadoItem[]> = computed(() => {
+    if (this.data_inicial() && this.data_fim()) {
+      return this.chamadoService.listarFiltroData(
+        this.data_inicial(),
+        this.data_fim()
+      );
+    } else {
+      return this.chamadoService.listarEmOrdemCrescente();
+    }
+  });
+
+  mudarFiltro(novoFiltro: 'HOJE' | 'DATA' | 'TODOS'): void {
+    switch (novoFiltro) {
+      case 'HOJE':
+        this.data_inicial.set(new Date().toDateString());
+        this.data_fim.set(new Date().toDateString());
+        this.opcaoData.set('HOJE');
+        break;
+      case 'DATA':
+        this.range.reset();
+        this.opcaoData.set('DATA');
+        break;
+      case 'TODOS':
+        this.data_inicial.set('');
+        this.data_fim.set('');
+        this.opcaoData.set('TODOS');
+    }
   }
 
-  atualizarTela(): void {
-    this.chamadosMock.set(this.chamadoService.listarTodos());
+  mudancaDatepicker(range: any) {
+    if (range.value.start && range.value.end) {
+      this.data_inicial.set(range.value.start);
+      this.data_fim.set(range.value.end);
+    }
   }
 }
