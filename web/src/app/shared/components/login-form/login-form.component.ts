808ac921654210ad88052edf41a82c633ec64c50
Angarius <angelo.prebianca@gmail.com>
2025-09-14 19:50:15 -0300

    feat: :lipstick: Pequenos ajustes css na interface de funcionario

--- web/src/app/shared/components/login-form/login-form.component.ts
+++ web/src/app/shared/components/login-form/login-form.component.ts
@@ -1,10 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink } from '@angular/router';
+import { UserRole } from '@core/store/user-role/user-role.store';
 
 @Component({
   selector: 'app-login-form',
@@ -16,4 +17,5 @@ import { RouterLink } from '@angular/router';
 export class LoginFormComponent {
   email = '';
   password = '';
+  readonly userRole = inject(UserRole);
 }
