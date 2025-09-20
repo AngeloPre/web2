aa2e659b15e259ab19d9fe735d8fc231a244090a
Henrique <henriquekkrrr@gmail.com>
2025-09-20 18:25:29 -0300

    provider pt-BR para money

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,23 +1,34 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  DEFAULT_CURRENCY_CODE,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
-import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+import {
+  MAT_DATE_LOCALE,
+  provideNativeDateAdapter,
+} from '@angular/material/core';
+import ptBr from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(ptBr);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
-  provideRouter(routes),
-  provideHttpClient(
-    withInterceptors([
-
-    ])
-  ),
-  provideStore(),
-  provideEnvironmentNgxMask(),
-  provideNativeDateAdapter(),
-  { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
-  ]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([])),
+    provideStore(),
+    provideEnvironmentNgxMask(),
+    provideNativeDateAdapter(),
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
+    { provide: LOCALE_ID, useValue: 'pt' },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'BRL' },
+  ],
 };
