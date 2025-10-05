c5948bc01e79cc4f7b62126cf37771743e2c18aa
Angarius <angelo.prebianca@gmail.com>
2025-10-05 01:11:53 -0300

    Revert "feature/api-auth"

--- api/src/main/java/br/ufpr/api/ApiApplication.java
+++ api/src/main/java/br/ufpr/api/ApiApplication.java
@@ -2,13 +2,12 @@ package br.ufpr.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class ApiApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
+
 }
