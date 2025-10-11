21e102a3d16eaa7e6de14a52ff154d292ddd2676
Henrique <henriquekkrrr@gmail.com>
2025-10-11 18:11:50 -0300

    agora sem auth

--- api/src/main/java/br/ufpr/api/config/SecurityConfig.java
+++ api/src/main/java/br/ufpr/api/config/SecurityConfig.java
@@ -3,12 +3,9 @@ package br.ufpr.api.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,17 +17,15 @@ public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        if (csrfDisabled) {
-            http.csrf(AbstractHttpConfigurer::disable);
-        }
         return http
+            .csrf(csrf -> csrf.disable())
             .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
-                .requestMatchers(HttpMethod.POST, "/categoria-equipamento").hasRole("ADMIN")
-                .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults())
+            //.authorizeHttpRequests(auth -> auth
+                //.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+                //.requestMatchers(HttpMethod.POST, "/categoria-equipamento").hasRole("ADMIN")
+                //.anyRequest().authenticated()
+            //)
+            //.httpBasic(Customizer.withDefaults())
             .build();
     }
 }
