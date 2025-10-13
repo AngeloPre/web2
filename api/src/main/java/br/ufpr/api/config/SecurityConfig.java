ab0aeaf5b3ead3b944fef57e4101e97526ca0076
Henrique <henriquekkrrr@gmail.com>
2025-10-13 09:40:35 -0300

    arrumar permissoes api cat equipamento

--- api/src/main/java/br/ufpr/api/config/SecurityConfig.java
+++ api/src/main/java/br/ufpr/api/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
                 .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                 .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                 .requestMatchers(HttpMethod.GET, "/auth/test").hasAnyAuthority("CLIENTE", "FUNCIONARIO")
-                .requestMatchers("/categoria-equipamento").permitAll()
+                .requestMatchers("/categoria-equipamento/**").permitAll()
                 .anyRequest().authenticated()
             )
             .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
