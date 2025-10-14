dcbb11b9e0b10554b50e545241c02407e6106232
João Mota <jvmotadev67@gmail.com>
2025-10-14 10:39:58 -0300

    cliente service

--- api/src/main/java/br/ufpr/api/service/ClienteService.java
+++ api/src/main/java/br/ufpr/api/service/ClienteService.java
@@ -1,47 +1,72 @@
 package br.ufpr.api.service;
 
+import br.ufpr.api.dto.ClienteRegisterDTO;
 import br.ufpr.api.model.entity.Cliente;
+import br.ufpr.api.model.enums.RoleUsuario;
 import br.ufpr.api.repository.ClienteRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class ClienteService {
-    @Autowired
-    ClienteRepository clientRepository;
+
+    private final ClienteRepository clienteRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final EmailService emailService;
 
     @Transactional
-    public Cliente save(Cliente client) {
-        return clientRepository.save(client);
+    public Cliente registrarCliente(ClienteRegisterDTO data) {
+        if(clienteRepository.existsByCpf(data.cpf())){
+            throw new IllegalArgumentException("CPF ja cadastrado");
+        }
+        if(clienteRepository.existsByEmail(data.email())){
+            throw new IllegalArgumentException("Email ja cadastrado");
+        }
+        
+        String senhaRandom = String.valueOf(new Random().nextInt(9000) + 1000);
+
+        Cliente cliente = new Cliente();
+        cliente.setNome(data.nome());
+        cliente.setCpf(data.cpf());
+        cliente.setEmail(data.email());
+        cliente.setTelefone(data.telefone());
+        cliente.setRole(RoleUsuario.CLIENTE);
+        cliente.setSenha(passwordEncoder.encode(senhaRandom));
+        cliente.setCep(data.cep());
+        cliente.setLogradouro(data.logradouro());
+        cliente.setComplemento(data.complemento());
+        cliente.setBairro(data.bairro());
+        cliente.setCidade(data.cidade());
+        cliente.setUf(data.uf());
+        cliente.setNumero(data.numero());
+
+        Cliente clienteSaved = clienteRepository.save(cliente);
+        emailService.enviarSenhaDeCadastro(clienteSaved.getEmail(), senhaRandom);
+
+        return clienteSaved;
     }
     @Transactional
     public void deleteById(Long id) {
-        clientRepository.deleteById(id);
+        clienteRepository.deleteById(id);
     }
     public List<Cliente> findAll() {
-        return clientRepository.findAll();
+        return clienteRepository.findAll();
     }
     public Optional<Cliente> findById(Long id) {
-        return clientRepository.findById(id);
+        return clienteRepository.findById(id);
     }
     public UserDetails findByEmail(String email) {
-        return clientRepository.findByEmail(email);
+        return clienteRepository.findByEmail(email);
     }
-
     public boolean existsById(Long id) {
-        return clientRepository.existsById(id);
-    }
-    public boolean existsByCpf(String cpf) {
-        return clientRepository.existsByCpf(cpf);
-    }
-    public boolean existsByEmail(String email) {
-        return clientRepository.existsByEmail(email);
+        return clienteRepository.existsById(id);
     }
 }
