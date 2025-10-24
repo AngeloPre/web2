f0a1e27601274aca9b1929f903f08ab12b6ea93b
Henrique <henriquekkrrr@gmail.com>
2025-10-23 23:49:45 -0300

    Mais exceções mapeadas

--- api/src/main/java/br/ufpr/api/exception/GlobalExceptionHandler.java
+++ api/src/main/java/br/ufpr/api/exception/GlobalExceptionHandler.java
@@ -1,11 +1,24 @@
 package br.ufpr.api.exception;
 
+import org.springframework.http.HttpMessage;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgument(IllegalArgumentException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST); // ERRO 400
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleNotReadable(HttpMessageNotReadableException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST); // ERRO 400
+    }
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<String> handleApiException(ApiException ex) {
