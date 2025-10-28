aedd4ffed352baa69aba497e13a97db0b63d3a23
Angarius <angelo.prebianca@gmail.com>
2025-10-28 18:45:05 -0300

    fix: Loading

--- web/src/app/pages/pag-inicial-funcionario/pag-inicial-funcionario.component.ts
+++ web/src/app/pages/pag-inicial-funcionario/pag-inicial-funcionario.component.ts
@@ -1,11 +1,11 @@
 import { ChamadoCardComponent } from '@shared/components/chamado-card/chamado-card.component';
 import { RiveLoaderComponent } from '@shared/components/rive-loader/rive-loader.component';
-import { Component, computed, effect, inject, OnInit, signal } from '@angular/core';
+import { Component, computed, effect, inject } from '@angular/core';
 import { StatusConsertoEnum } from '@model/enums/chamado-status.enum';
 import { ChamadoItem } from '@model/chamado.type';
 //Temporário
 import { ChamadoService } from '@services/chamado.service';
-import { FuncionarioService } from '@/app/services/funcionario-service';
+import { FuncionarioService } from '@/app/services/funcionario.service';
 
 @Component({
   selector: 'app-pag-inicial-funcionario',
@@ -16,7 +16,7 @@ import { FuncionarioService } from '@/app/services/funcionario-service';
 export class PagInicialFuncionarioComponent {
   private chamadoService = inject(ChamadoService);
   private funcionarioService = inject(FuncionarioService);
-  loading = this.chamadoService.loading || this.funcionarioService.loading;
+  loading = computed(() => this.chamadoService.loading() || this.funcionarioService.loading());
 
   constructor() {
     effect(() => {
