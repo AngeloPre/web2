e4bff31e52d01f7ae65415ebf225272a733c2b4d
Mario <mariom_mmn@hotmail.com>
2025-10-28 18:27:00 -0300

    fix

--- web/src/app/shared/components/funcionarios-table/funcionarios-table.component.ts
+++ web/src/app/shared/components/funcionarios-table/funcionarios-table.component.ts
@@ -1,10 +1,8 @@
-import { Usuario } from '@model/usuario';
 import { StatusAtivoInativoComponent } from "../status-ativo-inativo/status-ativo-inativo.component";
-import { Component, computed, effect, inject, OnInit, signal, input, output } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { ConfirmarModalComponent } from '../confirmar-modal/confirmar-modal.component';
 import { MatDialog } from '@angular/material/dialog';
-import { UsuarioService } from '@services/usuario.service';
 import { Funcionario } from '@/app/model/funcionario';
 import { FuncionarioService } from '@/app/services/funcionario-service';
 
@@ -17,22 +15,32 @@ import { FuncionarioService } from '@/app/services/funcionario-service';
 export class FuncionariosTableComponent {
   private dialog = inject(MatDialog);
   private funcionarioService = inject(FuncionarioService);
-  funcionarios = input.required<Usuario[]>();
+  funcionarios = input.required<Funcionario[]>(); // use Funcionario type
   router = inject(Router);
   deleted = output<number>();
 
   confirmarExcluir(id: number) {
-    const funcionario = this.funcionarioService.buscarPorId(id);
-    if (!funcionario) return;
+    this.funcionarioService.buscarPorId(id).subscribe({
+      next: (funcionario) => {
+        if (!funcionario) return;
 
-    this.dialog.open(ConfirmarModalComponent, {
-      data: { titulo: `Deseja excluir o(a) funcionário(a)`, confirmacao: 'Excluir' },
-      width: '360px'
-    }).afterClosed().subscribe(ok => {
-      if (!ok) return;
+        const dialogRef = this.dialog.open(ConfirmarModalComponent, {
+          data: { titulo: `Deseja excluir o(a) funcionário(a) ${funcionario.nome}?`, confirmacao: 'Excluir' },
+          width: '360px'
+        });
 
-      //this.funcionarioService.remover(funcionario)
-      this.deleted.emit(id);
+        dialogRef.afterClosed().subscribe(ok => {
+          if (!ok) return;
+
+          this.funcionarioService.remover(id).subscribe({
+            next: () => this.deleted.emit(id),
+            error: (err) => console.error('Erro ao remover funcionário', err)
+          });
+        });
+      },
+      error: (err) => {
+        console.error('Erro ao buscar funcionário', err);
+      }
     });
   }
 }
