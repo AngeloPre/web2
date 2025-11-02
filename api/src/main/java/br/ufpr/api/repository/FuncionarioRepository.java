45e833a03f878902f7a8b2266315b4be667da93a
Henrique <henriquekkrrr@gmail.com>
2025-11-02 00:28:11 -0300

    DTO no backend

--- api/src/main/java/br/ufpr/api/repository/FuncionarioRepository.java
+++ api/src/main/java/br/ufpr/api/repository/FuncionarioRepository.java
@@ -3,7 +3,9 @@ package br.ufpr.api.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.ufpr.api.model.entity.Funcionario;
+import java.util.List;
+
 
 public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer>{
-    
+    List<Funcionario> findByStatus(boolean status);
 }
