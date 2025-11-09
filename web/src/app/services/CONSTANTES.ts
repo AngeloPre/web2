ac70c9e29ef33dfd44246567a657f3ab153d65e2
Henrique <henriquekkrrr@gmail.com>
2025-11-09 19:42:30 -0300

    fix api url

--- web/src/app/services/CONSTANTES.ts
+++ web/src/app/services/CONSTANTES.ts
@@ -1,3 +1,3 @@
 //alternar conforme necessidade (local ou remoto)
-export const API_URL: string = 'http://localhost:8080';
-//export const API_URL: string = 'https://java-web2.tail041186.ts.net';
+//export const API_URL: string = 'http://localhost:8080';
+export const API_URL: string = 'https://java-web2.tail041186.ts.net';
