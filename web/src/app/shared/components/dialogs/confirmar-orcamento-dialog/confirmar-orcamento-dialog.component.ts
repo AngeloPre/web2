a7743e8f23257ca292dc3ea65834eb23457ff4cf
Henrique <henriquekkrrr@gmail.com>
2025-11-21 21:46:35 -0300

    Correções do fluxo de orçamento

--- web/src/app/shared/components/dialogs/confirmar-orcamento-dialog/confirmar-orcamento-dialog.component.ts
+++ web/src/app/shared/components/dialogs/confirmar-orcamento-dialog/confirmar-orcamento-dialog.component.ts
@@ -4,24 +4,37 @@ import { ChamadoService } from '@services/chamado.service';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-confirmar-orcamento-dialog',
-  imports: [MatDialogModule, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule],
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    CurrencyPipe,
+  ],
   templateUrl: './confirmar-orcamento-dialog.component.html',
-  styles: ``
+  styles: ``,
 })
 export class ConfirmarOrcamentoDialogComponent {
-  private chamadoService = inject(ChamadoService);
-  public dados: { chamado: ChamadoItem, precoBase: number } = inject(MAT_DIALOG_DATA)
-  comentario = '';
+  public dados: { chamado: ChamadoItem; precoBase: number } =
+    inject(MAT_DIALOG_DATA);
 
-  constructor(public dialogRef: MatDialogRef<ConfirmarOrcamentoDialogComponent>) { }
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmarOrcamentoDialogComponent>
+  ) {}
 
   salvarOrcamento() {
-    this.dialogRef.close({ confirmado: true, comentario: this.comentario });
+    this.dialogRef.close({ confirmado: true });
   }
 }
